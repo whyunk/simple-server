ed8994302b821af9aede6520eb9de76930cffdf8
Joseph Kim <whyunk79@gmail.com>
2023-11-15 14:28:35 +0900

    feat: server socket response a simple html

--- src/main/java/httpserver/HttpServer.java
+++ src/main/java/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package httpserver;
 import httpserver.config.Configuration;
 import httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * Driver Class for the Http Server
@@ -19,5 +25,37 @@ public class HttpServer {
 
         System.out.println("Using Port: " + currentConfiguration.getPort());
         System.out.println("Using Webroot: " + currentConfiguration.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(currentConfiguration.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Simple Java HTTP Server</title>" +
+                    "</head><body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
+
+            final String CRLF = "\n\r"; // 13, 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF +  //status Line : HTTP VERSION RESPONSE CODE_RESPONSE_MESSAGE
+                    "Content-Length: " + html.getBytes().length + CRLF +  //HEADER
+                            CRLF +
+                            html +
+                            CRLF + CRLF ;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
